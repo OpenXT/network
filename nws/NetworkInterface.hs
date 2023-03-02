024c7db90d6283ece841049733141b9431d397f1
Nicholas Tsirakis <tsirakisn@ainfosec.com>
2023-03-02 09:46:09 -0500

    [ghc8] Add missing language pragmas
    Signed-off-by: Nicholas Tsirakis <tsirakisn@ainfosec.com>

--- nws/NetworkInterface.hs
+++ nws/NetworkInterface.hs
@@ -16,7 +16,7 @@
 -- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 --
 
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ScopedTypeVariables, FlexibleContexts #-}
 module NetworkInterface where
 
 import Data.Int
